08df22bad7575d940f0ca1a0217af83b0f87ed24
tchn11 <akonany11@gmail.com>
2023-02-12 14:11:32 +0300

    Initial commit

---
+++ src/main/java/lab1/FirstTask.java
@@ -0,0 +1,27 @@
+package lab1;
+
+import java.lang.Math;
+
+public class FirstTask {
+    public static double arcsin(double x){
+        if (Math.abs(x) > 1)
+            return Double.NaN;
+
+        double answer = x;
+        double sum = 1;
+        double n = 2;
+        double a = 1;
+        double b = 2;
+
+        while (Math.abs(sum) > 0.0001)
+        {
+            sum = ((double)a / b) * (Math.pow(x, 2 * n - 1) / (2 * n - 1));
+            a *= 2L * n - 1;
+            b *= 2L * n;
+            n += 1L;
+            answer += sum;
+        }
+
+        return answer;
+    }
+}
---
+++ src/test/java/task1/tests.java
@@ -0,0 +1,74 @@
+package task1;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import lab1.FirstTask;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class tests {
+    double delta = 0.01;
+
+    @ParameterizedTest
+    @ValueSource(doubles = {1.1, 5, 10})
+    void positiveBig(double x)
+    {
+        assertEquals(FirstTask.arcsin(x), Math.asin(x), delta);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0.9, 0.99, 0.8})
+    void positiveCloseToOne(double x)
+    {
+        assertEquals(FirstTask.arcsin(x), Math.asin(x), delta);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0.6, 0.5, 0.4})
+    void positiveMiddle(double x)
+    {
+        assertEquals(FirstTask.arcsin(x), Math.asin(x), delta);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0.1, 0.05, 0.001})
+    void positiveCloseToZero(double x)
+    {
+        assertEquals(FirstTask.arcsin(x), Math.asin(x), delta);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0.0, 0.001, -0.001})
+    void zero(double x)
+    {
+        assertEquals(FirstTask.arcsin(x), Math.asin(x), delta);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-0.1, -0.05, -0.001})
+    void negativeCloseToZero(double x)
+    {
+        assertEquals(FirstTask.arcsin(x), Math.asin(x), delta);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-0.6, -0.5, -0.4})
+    void negativeMiddle(double x)
+    {
+        assertEquals(FirstTask.arcsin(x), Math.asin(x), delta);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-0.9, -0.99, -0.8})
+    void negativeCloseToOne(double x)
+    {
+        assertEquals(FirstTask.arcsin(x), Math.asin(x), delta);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-1.1, -5, -10})
+    void negativeBig(double x)
+    {
+        assertEquals(FirstTask.arcsin(x), Math.asin(x), delta);
+    }
+}
