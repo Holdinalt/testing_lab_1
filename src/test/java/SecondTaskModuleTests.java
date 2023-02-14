129ca3d10bac94bf2cfc252e4e60f7a23f7a541b
Holdest White <zbsholdest@mail.ru>
2023-02-14 15:35:11 +0300

    Made OOP third task

--- src/test/java/SecondTaskModuleTests.java
+++ src/test/java/SecondTaskModuleTests.java
@@ -1,9 +1,81 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import lab1.BinomialHeap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.time.Duration;
 class SecondTaskModuleTests {
+
+    static BinomialHeap binHeap;
+
+    @Test
+    void Insertable(){
+        binHeap = new BinomialHeap();
+        binHeap.insert(12);
+        assertEquals(binHeap.getSize(), 1);
+        assertArrayEquals(binHeap.getArray(), new int[]{12});
+    }
+
+    @Nested
+    public class Insertable{
+
+        @BeforeEach
+        void initEach(){
+            binHeap = new BinomialHeap();
+            binHeap.insert(12);
+            binHeap.insert(8);
+            binHeap.insert(5);
+            binHeap.insert(15);
+            binHeap.insert(7);
+        }
+
+        @Test
+        void HeapSortedAndGettable(){
+            assertArrayEquals(binHeap.getArray(), new int[]{7, 12, 8, 15, 5});
+        }
+
+        @Test
+        void SizeCorrect(){
+            assertEquals(binHeap.getSize(), 5);
+        }
+
+        @Test
+        void Clearable(){
+            binHeap.makeEmpty();
+            assertEquals(binHeap.getSize(), 0);
+            assertArrayEquals(binHeap.getArray(), new int[]{});
+        }
+
+        @Test
+        void FindMinTest(){
+            assertEquals(binHeap.findMinimum(), 5);
+        }
+
+        @Test
+        void Deletable(){
+            binHeap.delete(8);
+            assertArrayEquals(binHeap.getArray(), new int[]{15, 7, 12, 5});
+        }
+
+        @Test
+        void DecreaseWithoutMerge(){
+            binHeap.decreaseKeyValue(15, 9);
+            assertArrayEquals(binHeap.getArray(), new int[]{7, 12, 8, 9, 5});
+        }
+
+        @Test
+        void DecreaseWithMerge(){
+            binHeap.decreaseKeyValue(12, 7);
+            assertArrayEquals(binHeap.getArray(), new int[]{7, 8, 7, 15, 5});
+        }
+
+        @Test
+        void MinExtractable(){
+            binHeap.extractMin();
+            assertArrayEquals(binHeap.getArray(), new int[]{12, 8, 15, 7});
+        }
+
+    }
 
 
 
