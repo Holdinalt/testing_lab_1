eea4364d2d40429ddebfd37e609baaacf7435b3b
Holdest White <zbsholdest@mail.ru>
2023-02-20 12:07:30 +0300

    remove 3 task main

--- src/main/java/lab1/task3/ThirdTask.java
+++ src/main/java/lab1/task3/ThirdTask.java
@@ -1,22 +1,22 @@
-package lab1.task3;
-
-import lab1.task3.Entities.*;
-
-public class ThirdTask {
-
-    public static void main(String[] args) {
+//package lab1.task3;
+//
+//import lab1.task3.Entities.*;
+//
+//public class ThirdTask {
+//
+//    public static void main(String[] args) {
+////        Surroundings surroundings = new Surroundings();
+////        surroundings.addToCornerEntity(new They());
+////        ComputerBank computerBank = new ComputerBank();
+////
+////        System.out.println("<--- Начало текста --->");
+////
+////        Bombing bombing = new Bombing(surroundings);
+////        bombing.giveDamage(computerBank, 30);
 //        Surroundings surroundings = new Surroundings();
-//        surroundings.addToCornerEntity(new They());
 //        ComputerBank computerBank = new ComputerBank();
-//
-//        System.out.println("<--- Начало текста --->");
-//
 //        Bombing bombing = new Bombing(surroundings);
-//        bombing.giveDamage(computerBank, 30);
-        Surroundings surroundings = new Surroundings();
-        ComputerBank computerBank = new ComputerBank();
-        Bombing bombing = new Bombing(surroundings);
-        bombing.giveDamage(computerBank, 15);
-        computerBank.printLife();
-    }
-}
+//        bombing.giveDamage(computerBank, 15);
+//        computerBank.printLife();
+//    }
+//}
